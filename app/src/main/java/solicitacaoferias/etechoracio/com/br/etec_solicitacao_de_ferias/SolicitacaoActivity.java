0dc116e56616f48b34e4bf1096db654fd00bb4b2
giovane358 <giovanesilva.etec@gmail.com>
2019-06-18 22:35:11 -0300

    giovane

---
+++ app/src/main/java/solicitacaoferias/etechoracio/com/br/etec_solicitacao_de_ferias/SolicitacaoActivity.java
@@ -0,0 +1,13 @@
+package solicitacaoferias.etechoracio.com.br.etec_solicitacao_de_ferias;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SolicitacaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solicitacao);
+    }
+}
